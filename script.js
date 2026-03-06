acaaee75f95d600f4a996a41fa97ca5c30ba3d16
sonuprajapati9794 <prajapati979473@gmail.com>
2026-03-06 15:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-//your JS code here. If required.
 function secondHighest(arr) {
 
     if (arr.length < 2) {
         return -Infinity;
     }
 
-    let max = -Infinity;
-    let secondMax = -Infinity;
+    let unique = [...new Set(arr)];  // duplicate remove
 
-    for (let i = 0; i < arr.length; i++) {
-
-        if (arr[i] > max) {
-            secondMax = max;
-            max = arr[i];
-        }
-        else if (arr[i] > secondMax && arr[i] !== max) {
-            secondMax = arr[i];
-        }
+    if (unique.length < 2) {
+        return -Infinity;
     }
 
-    return secondMax;
+    unique.sort((a, b) => b - a);  // descending order
+
+    return unique[1]; // second highest
 }
